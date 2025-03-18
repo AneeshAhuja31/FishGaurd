0c166bd45b1182a589967789097c1ad387d182e8
Aneesh Ahuja <aneeshahuja31@gmail.com>
2025-03-19 02:56:36 +0530

    uneccessary commit

--- extensions/background.js
+++ extensions/background.js
@@ -1,2 +1,44 @@
-let API_url = "https://fishgaurd.com";
-
+chrome.runtime.onInstalled.addListener(async () => {
+    console.log('FishGuard extension installed');
+    
+    // Initialize storage
+    await chrome.storage.local.set({
+      cachedUrls: {},
+      cachedJobs: {},
+      statistics: {
+        phishingDetected: 0,
+        suspiciousJobs: 0,
+        maliciousJobs: 0
+      }
+    });
+  });
+  
+  // Listen for messages from content scripts
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'updateStats') {
+      chrome.storage.local.get(['statistics'], (result) => {
+        const stats = result.statistics || {
+          phishingDetected: 0,
+          suspiciousJobs: 0,
+          maliciousJobs: 0
+        };
+        
+        if (message.data.phishingDetected) {
+          stats.phishingDetected++;
+        }
+        
+        if (message.data.jobStatus === 'suspicious') {
+          stats.suspiciousJobs++;
+        }
+        
+        if (message.data.jobStatus === 'malicious') {
+          stats.maliciousJobs++;
+        }
+        
+        chrome.storage.local.set({ statistics: stats });
+      });
+    }
+    
+    return true;
+  });
+  
